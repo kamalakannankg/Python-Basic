2ce93f6e249b0d8cc475fec3ab4e346ecac237e6
kamalakannankg <kamalakannankg1@gmail.com>
2025-12-12 00:36:16 +0530

    casting

---
+++ casting.py
@@ -0,0 +1,30 @@
+#Casting
+#when you want to specify a type on to a variable. 
+# This can be done with casting
+
+#Intergers
+x=int(3)
+y=int(85.2)
+z=int("5")
+print(x)
+print(y)
+print(z)
+
+
+#Floats
+x=float(5)
+y=float(5.54)
+z=float("3")
+w=float("54.1")
+print(x)
+print(y)
+print(z)
+print(w)
+
+#strings
+x=str("s1")
+y=str(1)
+z=str(6.1)
+print(x)
+print(y)
+print(z)
